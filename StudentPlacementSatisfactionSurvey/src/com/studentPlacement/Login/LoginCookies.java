cede5ffe00dde7e446da1a2cfd6d2fe346d89220
Solange Yehouenou <solange.yehouenou@yahoo.co.uk>
2020-11-30 12:15:22 +0000

    Added 3 classes LoginCookies, LoginCookies2 and LoginCookies3.java

---
+++ StudentPlacementSatisfactionSurvey/src/com/studentPlacement/Login/LoginCookies.java
@@ -0,0 +1,54 @@
+package com.studentPlacement.Login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginCookies
+ */
+@WebServlet("/LoginCookies")
+public class LoginCookies extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginCookies() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("html");
+		out.println("<form action action = 'http://localhost:3030/StudentPlacementSatisfactionSurvey/LoginCookies2'>");
+		out.println("Name: <input type = 'text' name = 'txtName'><br><br>");
+		out.println("Email: <input type = 'text' name = 'txtEmail'><br><br>");
+		out.println("<input type = 'submit' value = 'LOGIN'>");
+		out.println("</form>");
+		out.println("</html>");
+	}
+		
+		
+		
+		/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ StudentPlacementSatisfactionSurvey/src/com/studentPlacement/Login/LoginCookies2.java
@@ -0,0 +1,41 @@
+package com.studentPlacement.Login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginCookies2
+ */
+@WebServlet("/LoginCookies2")
+public class LoginCookies2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginCookies2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
